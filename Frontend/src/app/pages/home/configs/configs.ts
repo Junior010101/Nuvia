1bc06d233e7dda1abe179e5de1d16d26ca235dbe
ViniciusRodrigues0079 <vinicius.rgmp.escolar@gmail.com>
2025-08-28 14:43:35 -0300

    mensagem

---
+++ Frontend/src/app/pages/home/configs/configs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-configs',
+  imports: [],
+  templateUrl: './configs.html',
+  styleUrl: './configs.css'
+})
+export class Configs {
+
+}
