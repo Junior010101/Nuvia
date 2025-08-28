70e2c937e37d843712540484bc9a2d8e41ff9ed5
junior010101 <marcondesjunior2209@gmail.com>
2025-08-27 23:19:57 -0300

    Arrumando componente

---
+++ Frontend/src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+  constructor(public router: Router) {}
+}
--- Frontend/src/app/pages/home/home.ts
+++ Frontend/src/app/pages/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Sidebar],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
